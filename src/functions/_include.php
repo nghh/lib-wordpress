c4f1954225947510fe61a8e21c6cd63fda1701e0
Jan Reiland <moin@nordgestalten.com>
2021-12-07 14:12:38 +0100

    added camel case helper function

--- src/functions/_include.php
+++ src/functions/_include.php
@@ -1,1 +1,7 @@
 <?php
+namespace Nghh\Lib\Wordpress\func;
+
+// Require all functions
+foreach (glob(__DIR__ . '/*.inc.php') as $file) {
+    require $file;
+}
---
+++ src/functions/helper.inc.php
@@ -0,0 +1,17 @@
+<?php
+
+if(function_exists('camel_case')) {
+    function camel_case($str, array $noStrip = [])
+    {
+        // non-alpha and non-numeric characters become spaces
+        $str = preg_replace('/[^a-z0-9' . implode("", $noStrip) . ']+/i', ' ', $str);
+        $str = trim($str);
+        $str = str_replace(['-', '_', '–'], " ", $str);
+        // uppercase the first character of each word
+        $str = ucwords($str);
+        $str = str_replace(" ", "", $str);
+        $str = lcfirst($str);
+
+        return $str;
+    }
+}
